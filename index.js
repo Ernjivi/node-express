bfb4dfedca12c8f2a0b20792007ed0604d72107b
Ernesto Jiménez <ernesto@bedu.org>
2019-01-14 12:23:20 -0600

    Added diferent methods to the same route

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
-})
+});
+
+app.post('/', (req, res) => {
+  res.send('Es una petición POST');
+});
+
+app.put('/', (req, res) => {
+  res.send('Es una petición PUT');
+});
+
+app.delete('/', (req, res) => {
+  res.send('Es una petición DELETE');
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
